5438a679496bd23b44a17e992a3a96363c0b73cb
rhshimulglobalitsolutions <rhshimul@globalitsolutions.co.jp>
2023-06-23 17:17:51 +0900

    Second and Updated

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
-use App\Http\Controllers\SearchController;
+
 
 
 /*
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SurveyController;
-use App\Models\Survey;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
